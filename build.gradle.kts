828dc6119896e1a3a305b64143cf62636d2ea60d
ToGetAUsername <59630739+ToGetAUsername@users.noreply.github.com>
2025-02-19 23:09:10 +1100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+	id("fabric-loom") version "1.10-SNAPSHOT"
+}
+
+version = project.property("mod_version") as String
+group = project.property("maven_group") as String
+
+base {
+	archivesName.set(project.property("archives_base_name") as String)
+}
+
+val targetJavaVersion = 21
+java {
+	val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+	if (JavaVersion.current() < javaVersion) {
+		toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+	}
+	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+	// if it is present.
+	// If you remove this line, sources will not be generated.
+	withSourcesJar()
+}
+
+repositories {
+	maven("https://maven.terraformersmc.com/releases/")
+}
+
+dependencies {
+	// To change the versions see the gradle.properties file
+	minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+	mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+	modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+
+	modImplementation("com.terraformersmc:modmenu:${project.property("modmenu_version")}")
+	// for testing only
+	modRuntimeOnly("net.fabricmc.fabric-api:fabric-api:0.116.1+1.21.4")
+}
+
+tasks {
+	processResources {
+		val props = mapOf("version" to project.version) + keyToPropertyMap(
+			"minecraft_version",
+			"loader_version",
+			"modmenu_version"
+		)
+		inputs.properties(props)
+		filteringCharset = "UTF-8"
+
+		filesMatching("fabric.mod.json") {
+			expand(props)
+		}
+	}
+
+	withType<JavaCompile>().configureEach {
+		// ensure that the encoding is set to UTF-8, no matter what the system default is
+		// this fixes some edge cases with special characters not displaying correctly
+		// see http://yodaconditions.net/blog/fix-for-java-file-encoding-problems-with-gradle.html
+		// If Javadoc is generated, this must be specified in that task too.
+		options.encoding = "UTF-8"
+		if(targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+			options.release.set(targetJavaVersion)
+		}
+	}
+
+	jar {
+		from("LICENSE") {
+			rename { "${it}_${project.base.archivesName}"}
+		}
+	}
+}
+
+fun keyToPropertyMap(vararg keys: String): Map<String, Any?> = buildMap {
+	for(key in keys) put(key, project.property(key))
+}
---
+++ src/main/java/togetausername/hideserverips/mixin/TextFieldWidgetMixin.java
@@ -0,0 +1,30 @@
+package togetausername.hideserverips.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.AddServerScreen;
+import net.minecraft.client.gui.screen.multiplayer.DirectConnectScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.text.TranslatableTextContent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import togetausername.hideserverips.ConfigScreen;
+
+@Mixin(TextFieldWidget.class)
+public abstract class TextFieldWidgetMixin {
+	@ModifyExpressionValue(
+		method = "renderWidget",
+		at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;text:Ljava/lang/String;")
+	)
+	private String censorText(String original) {
+		Screen screen = MinecraftClient.getInstance().currentScreen;
+		if(((TextFieldWidget) (Object) this).getMessage().getContent() instanceof TranslatableTextContent content
+			&& content.getKey().equals("addServer.enterIp")
+			&& !ConfigScreen.shouldShowIps()
+			&& (screen instanceof AddServerScreen || screen instanceof DirectConnectScreen))
+				return "*".repeat(original.length());
+
+		return original;
+	}
+}
